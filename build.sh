14511fcc55098f79148a10eddfb81c78f302d0d1
gianluca-mascolo <gianluca.mascolo@bitrock.it>
2020-01-30 00:41:20 +0100

    githash version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /opt/makepkg/rpm
 find . -type f -maxdepth 1 -path '*rpm' -exec rm -f '{}' ';'
-fpm -s dir -t rpm --rpm-os linux  -v ${PKG_VERSION} --iteration 0 -n ${PKG_NAME}  -C /opt/makepkg/pkg
+fpm -s dir -t rpm --rpm-os linux -v ${PKG_VERSION} --iteration ${GIT_HASH} -n ${PKG_NAME}  -C /opt/makepkg/pkg
 exit $?
